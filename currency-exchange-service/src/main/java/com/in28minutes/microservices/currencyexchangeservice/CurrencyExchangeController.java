21dbeaf5587f8479ae0118ec993f8fa361af4e96
Chetan Singh1 <chetan.singh1@synapse.com>
2018-09-21 00:14:18 +0530

    Setup JPA and define cool method to retrieve data using jpa repository

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -12,9 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class CurrencyExchangeController {
 	@Autowired
 	Environment environment;
+	
+	@Autowired
+	ExchangeValueRepository exchangeRepo;
+	
 	@GetMapping("/exchange/from/{from}/to/{to}")
 	public ExchangeValue retrieveExchangeValue(@PathVariable String from,@PathVariable  String to) {
-		ExchangeValue ev = new  ExchangeValue(10l, from, to, BigDecimal.valueOf(65L));
+		ExchangeValue ev = exchangeRepo.findByFromAndTo(from, to);
 		ev.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
 		return ev;
 	}
